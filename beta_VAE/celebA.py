a53fdc2a1361c0bf27602a0612336ec2c7bf7ebf
lidonghao <leedonhom@gmail.com>
2021-07-28 19:19:21 -0400

    gi

--- beta_VAE/celebA.py
+++ beta_VAE/celebA.py
@@ -34,15 +34,12 @@ class CelebA():
                 self.attributes = self.attributes.drop(feature, axis=1)
                 self.num_features -= 1
 
-        self.attributes.set_index('image_id', inplace=True)
         self.attributes.replace(to_replace=-1, value=0, inplace=True)
-        self.attributes['image_id'] = list(self.attributes.index)
 
         self.features_name = list(self.attributes.columns)[:-1]
 
         # load ideal partitioning:
         self.partition = pd.read_csv(self.partition_path)
-        self.partition.set_index('image_id', inplace=True)
 
     def split(self, name='training', drop_zero=False):
         '''Returns the ['training', 'validation', 'test'] split of the dataset'''
@@ -59,7 +56,7 @@ class CelebA():
         partition = self.partition.drop(index=to_drop.index)
 
         # join attributes with selected partition:
-        joint = partition.join(self.attributes, how='inner').drop('partition', axis=1)
+        joint = partition.join(self.attributes.drop('image_id', axis=1), how='inner').drop('partition', axis=1)
 
         if drop_zero is True:
             # select rows with all zeros values
