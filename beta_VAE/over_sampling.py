adff01e1f27728d238a2c390ae6d56d962395fe5
lidonghao <leedonhom@gmail.com>
2021-07-29 10:44:39 -0400

    gi

--- beta_VAE/over_sampling.py
+++ beta_VAE/over_sampling.py
@@ -15,7 +15,7 @@ from loss import compute_loss, confidence_function, top_loss, acc_metrix
 def start_train(epochs, target, threshold, model, classifier, o_classifier,
                 train_set, majority_set, test_set, date, filePath):
     sim_optimizer = tf.keras.optimizers.Adam(1e-5)
-    cls_optimizer = tf.keras.optimizers.Adam(1e-6)
+    cls_optimizer = tf.keras.optimizers.Adam(1e-5)
     def train_step(model, classifier, o_classifier, x, y, sim_optimizer, cls_optimizer, oversample=False, threshold=None):
         if (oversample):
             with tf.GradientTape() as sim_tape, tf.GradientTape() as cls_tape:
