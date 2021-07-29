50eafaff984ed5b193c2cf461c7fc3c0e1d93932
lidonghao <leedonhom@gmail.com>
2021-07-29 13:31:42 -0400

    gi

--- beta_VAE/over_sampling.py
+++ beta_VAE/over_sampling.py
@@ -45,13 +45,14 @@ def start_train(epochs, target, threshold, model, classifier, o_classifier,
                 cls_optimizer.apply_gradients(zip(cls_gradients, o_classifier.trainable_variables))
 
         else:
-            with tf.GradientTape() as sim_tape, tf.GradientTape() as cls_tape:
+            with tf.GradientTape() as sim_tape, tf.GradientTape() as cls_tape, tf.GradientTape() as o_tape:
                 ori_loss, _, encode_loss = compute_loss(model, classifier, x, y)
             sim_gradients = sim_tape.gradient(ori_loss, model.trainable_variables)
             cls_gradients = cls_tape.gradient(encode_loss, classifier.trainable_variables)
+            o_gradients = o_tape.gradient(encode_loss, o_classifier.trainable_variables)
             cls_optimizer.apply_gradients(zip(cls_gradients, classifier.trainable_variables))
             sim_optimizer.apply_gradients(zip(sim_gradients, model.trainable_variables))
-            cls_optimizer.apply_gradients(zip(cls_gradients, o_classifier.trainable_variables))
+            cls_optimizer.apply_gradients(zip(o_gradients, o_classifier.trainable_variables))
     checkpoint_path = "./checkpoints/{}/{}".format(date, filePath)
     ckpt = tf.train.Checkpoint(sim_clr=model,
                                clssifier = classifier,
