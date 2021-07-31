59332fd74e6824f5d854adce5581ecf7690b85df
lidonghao <leedonhom@gmail.com>
2021-07-30 22:01:30 -0400

    gi

--- beta_VAE/over_sampling.py
+++ beta_VAE/over_sampling.py
@@ -100,12 +100,12 @@ def start_train(epochs, target, threshold, model, classifier, o_classifier,
 
 
 
-
+        '''
         for x, y in tf.data.Dataset.zip((train_set[0], train_set[1])):
             train_step(model, classifier, o_classifier, 
             x, y, sim_optimizer, cls_optimizer, oversample=True, threshold=threshold)
 
-
+        '''
 
         end_time = time.time()
         elbo_loss = tf.keras.metrics.Mean()
