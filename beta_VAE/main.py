3099e51b9eb49bf6d591dee9705c654204f86cf2
lidonghao <leedonhom@gmail.com>
2021-08-09 00:53:38 -0400

    gi

--- beta_VAE/main.py
+++ beta_VAE/main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     classifier = Classifier(shape=dataset.shape, model='mlp', num_cls=dataset.num_cls)
 
     checkpoint = tf.train.Checkpoint(sim_clr=model, clssifier=classifier)
-    checkpoint.restore("./checkpoints/8_5/pre_train_large_celebA_lsq/ckpt-40")
+    checkpoint.restore("./checkpoints/8_5/pre_train_mnist_lsq/ckpt-40")
 
     start_train(epochs, target, threshold_list, method, model, classifier, dataset,
                 [train_set, train_labels],
