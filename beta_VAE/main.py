f7db3458f7e7e51759cc627c239ec8abf1844421
lidonghao <leedonhom@gmail.com>
2021-08-12 12:28:42 -0400

    gi

--- beta_VAE/main.py
+++ beta_VAE/main.py
@@ -9,20 +9,20 @@ import numpy as np
 if __name__ == '__main__':
     os.environ["CUDA_DECICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = "1,4,5,7"
-    '''
+
     gpus = tf.config.list_physical_devices('GPU')
     if gpus:
         # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
         try:
             tf.config.set_logical_device_configuration(
                 gpus[0],
-                [tf.config.LogicalDeviceConfiguration(memory_limit=7168)])
+                [tf.config.LogicalDeviceConfiguration(memory_limit=8168)])
             logical_gpus = tf.config.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             # Virtual devices must be set before GPUs have been initialized
             print(e)
-    '''
+
     n = 5
     #threshold = [1. , 1., 0.957, 0.973, 0.964, 0.924, 0.927, 0.899, 0.739,0.744]
     threshold = [0.96, 0.927, 0.899, 0.739, 0.744]
--- beta_VAE/pre_train.py
+++ beta_VAE/pre_train.py
@@ -133,6 +133,20 @@ def start_train(epochs, c_epochs, model, classifier, method,
 if __name__ == '__main__':
     os.environ["CUDA_DECICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = "1,4,5,7"
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+        try:
+            tf.config.set_logical_device_configuration(
+                gpus[0],
+                [tf.config.LogicalDeviceConfiguration(memory_limit=8168)])
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
+
+    n = 5
     target = 'margin'
     threshold = 0.95
     date = '8_10'
