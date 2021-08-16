ab043f988237e6f3317139977d3c9507d8c94696
sd2880358 <leedonhom@gmail.com>
2021-08-16 17:48:07 -0400

    gi

--- beta_VAE/over_sampling.py
+++ beta_VAE/over_sampling.py
@@ -131,14 +131,14 @@ def start_train(epochs, n, threshold_list, method, model, classifier, dataset,
                 label_on_train = classifier_list[i].call(x).numpy().argmax(-1)
                 metrix_list[i]['train_acc'].append(np.sum(label_on_train==y.numpy())/len(y.numpy()))
                 test = np.linspace(-5, 5, 11)
-                features_set = np.zeros([11, features._shape[1] * features._shape[0], 7])
-                for features_idx in range(features._shape[1]):
+                features_set = np.zeros([11, features.shape[1] * features.shape[0], 7])
+                for features_idx in range(features.shape[1]):
                     for j in range(len(test)):
                         tmp = features.numpy().copy()
                         tmp[:, features_idx] = test[j]
-                        features_set[j, features_idx*features._shape[0]:(features_idx+1)*features._shape[0], :] = tmp
+                        features_set[j, features_idx*features.shape[0]:(features_idx+1)*features.shape[0], :] = tmp
                 features_set = tf.Variable(
-                    features_set.reshape(features._shape[0]*features._shape[1]*11, 7))
+                    features_set.reshape(features.shape[0]*features.shape[1]*11, 7))
                 for cls in range(1, model.num_cls):
                     # oversampling
                             sample_label = np.array(([cls] * features_set.shape[0]))
