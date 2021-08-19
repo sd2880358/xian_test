28927ce1a769c7158105779b7c43d5c31c40a15d
sd2880358 <leedonhom@gmail.com>
2021-08-18 21:16:22 -0400

    gi

--- beta_VAE/main.py
+++ beta_VAE/main.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
             # Virtual devices must be set before GPUs have been initialized
             print(e)
 
-    n = 10
-    batch_size = 64
-    threshold = [1.0, 0.95, 0.95, 0.95, 0.95, 0.95, 0.95, 0.95, 0.95, 0.95]
+    n = 5
+    batch_size = 32
+    threshold = [1.0, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9]
     #threshold = [0.96, 0.927, 0.899, 0.739, 0.744]
     threshold_list = [threshold]
     date = '8_18'
